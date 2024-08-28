e8d3bb4731879c45f8e2bc085db02ce398c81fef
KomfortKimko <komfortkimk@gmail.com>
2024-08-28 21:03:23 +0300

    Sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    if length == 1:
+        print([0])
+        return
+    
+    fibonacci_sequence = [0, 1]
+
+    for i in range(2, length):
+        next_num = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_num)
+
+    print(fibonacci_sequence)
